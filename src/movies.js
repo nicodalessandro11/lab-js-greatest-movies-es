64b72bce15e70f9ec01984ccdead6e8faf573a4e
nicodalessandro11 <nicodalessandro11@gmail.com>
2023-07-29 18:39:52 +0200

    lab review

--- src/movies.js
+++ src/movies.js
@@ -18,11 +18,13 @@ function howManyMovies(moviesArray) {
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
 function scoresAverage(moviesArray) {
-  return parseFloat((
-    moviesArray.reduce((acc, movie) => {
-      return acc + movie.score;
-    }, 0) / moviesArray.length
-  ).toFixed(2));
+  return parseFloat(
+    (
+      moviesArray.reduce((acc, movie) => {
+        return acc + movie.score;
+      }, 0) / moviesArray.length
+    ).toFixed(2)
+  );
 }
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
@@ -30,11 +32,13 @@ function dramaMoviesScore(moviesArray) {
   const dramaMovies = moviesArray.filter((movie) =>
     movie.genre.includes('Drama')
   );
-  return parseFloat((
-    dramaMovies.reduce((acc, movie) => {
-      return acc + movie.score;
-    }, 0) / moviesArray.length
-  ).toFixed(2));
+  return parseFloat(
+    (
+      dramaMovies.reduce((acc, movie) => {
+        return acc + movie.score;
+      }, 0) / moviesArray.length
+    ).toFixed(2)
+  );
 }
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
@@ -44,7 +48,9 @@ function orderByYear(moviesArray) {
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 function orderAlphabetically(moviesArray) {
-  return [...moviesArray].sort((a, b) => a.title.localeCompare(b.title)).slice(0, 20);
+  return [...moviesArray]
+    .sort((a, b) => a.title.localeCompare(b.title))
+    .slice(0, 20);
 }
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
@@ -57,7 +63,46 @@ function turnHoursToMinutes(moviesArray) {
 }
 
 // BONUS - Iteration 8: Best yearly score average - Best yearly score average
-function bestYearAvg(moviesArray) {}
+function bestYearAvg(moviesArray) {
+  if (moviesArray.length === 0) {
+    return null;
+  }
+
+  // Create an object to store the total score and count of movies for each year
+  const yearStats = {};
+
+  moviesArray.forEach((movie) => {
+    const year = movie.year;
+    const score = movie.score;
+
+    if (!yearStats[year]) {
+      // If the year does not exist in the yearStats object, initialize it with the first movie's score
+      yearStats[year] = { totalScore: score, movieCount: 1 };
+    } else {
+      // If the year already exists, add the current movie's score to the total score and increment the movie count
+      yearStats[year].totalScore += score;
+      yearStats[year].movieCount++;
+    }
+  });
+
+  // Calculate the average score for each year and find the best year
+  let bestYear = null;
+  let bestAverageScore = 0;
+
+  for (const year in yearStats) {
+    const averageScore =
+      yearStats[year].totalScore / yearStats[year].movieCount;
+
+    if (averageScore > bestAverageScore) {
+      bestYear = year;
+      bestAverageScore = averageScore;
+    }
+  }
+
+  return `The best year was ${bestYear} with an average score of ${bestAverageScore.toFixed(
+    2
+  )}`;
+}
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
